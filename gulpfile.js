546343d0d9cd55aac409994283be6986fde64adc
Sergey Timchenko <art-street93@mail.ru>
2016-09-11 18:48:58 +0300

    Tuned gulp

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const gulp    = require("gulp"),
+	  minify  = require("gulp-minify-css"),
+	  prefix  = require("gulp-autoprefixer"),
+	  sass    = require("gulp-sass"),
+	  rename  = require("gulp-rename"),
+	  babel   = require('gulp-babel'),
+	  uglify  = require('gulp-uglify');
+
+gulp.task("css", function() {
+	gulp.src("scss/main.scss")
+	.pipe(sass())
+	.pipe(prefix("last 5 versions","> 1%","ie 9"))
+	.pipe(minify())
+	.pipe(rename("main.min.css"))
+	.pipe(gulp.dest("css/"));
+});
+
+gulp.task("js", function() {
+	gulp.src("js/app/main.js")
+	.pipe(babel({presets: ["es2015"]}))
+	.pipe(uglify())
+	.pipe(rename("main.min.js"))
+	.pipe(gulp.dest("js/"));
+});
+
+gulp.task("watch", function() {
+	gulp.watch("scss/**/*.scss", ["scss"]);
+	gulp.watch("js/app/**/*.sass", ["js"]);
+});
+
+gulp.task("default", ["watch", "css", "js"]);
